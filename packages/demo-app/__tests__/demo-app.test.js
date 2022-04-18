2d0ee0a32e5d7f1272f77ceabb67f4f75c736e1b
Diego Pascual <diegopf@users.noreply.github.com>
2022-04-18 22:18:52 +0200

    silly commit

--- packages/demo-app/__tests__/demo-app.test.js
+++ packages/demo-app/__tests__/demo-app.test.js
@@ -1,5 +1,4 @@
-const sum = require('../lib/demo-app');
-
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+const sum = require("../lib/demo-app");
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
 });
